943c4cd308362098e1aac4950bdefaf121835260
Bio121 <daniel.alejandro121@gmail.com>
2021-10-23 04:04:37 -0500

    integrantes

---
+++ src/components/integrantesSquad.js
@@ -0,0 +1,40 @@
+import { Container, Typography } from '@material-ui/core';
+import React from 'react'
+import Avatar from '@material-ui/core/Avatar';
+
+const integrantesSquad = (x) => {
+    return (
+        <div>       
+            <Typography style={{fontSize: 40}}>
+                Integrantes
+            </Typography>
+            <div style = {{backgroundColor: "#5b5763", padding: 10, width: '15rem'}}>
+                <div style = {{display: 'inline-flex'}}>
+                    <Avatar alt="Remy Sharp" src="https://pbs.twimg.com/media/EiNYM5CWAAAh9PV?format=png&name=240x240" />
+                    <Typography style={{fontSize: 20, marginLeft: 7}}>
+                    polluelo
+                    </Typography>
+                </div>
+            </div>
+            <div style = {{backgroundColor: "#5b5763", padding: 10, width: '15rem'}}>
+                <div style = {{display: 'inline-flex'}}>
+                    <Avatar alt="Remy Sharp" src="https://pbs.twimg.com/media/EiNYM5CWAAAh9PV?format=png&name=240x240" />
+                    <Typography style={{fontSize: 20, marginLeft: 7}}>
+                    gallo
+                    </Typography>
+                </div>
+            </div>
+            <div style = {{backgroundColor: "#5b5763", padding: 10, width: '15rem'}}>
+                <div style = {{display: 'inline-flex'}}>
+                    <Avatar alt="Remy Sharp" src="https://pbs.twimg.com/media/EiNYM5CWAAAh9PV?format=png&name=240x240" />
+                    <Typography style={{fontSize: 20, marginLeft: 7}}>
+                    paloma
+                    </Typography>
+                </div>
+            </div>
+        </div>
+            
+    )
+}
+
+export default integrantesSquad;
