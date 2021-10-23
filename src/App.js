a424cf08aaa77b59aaeee6ccfeb9be70dfd017be
JesusMtzz <52387097+JesusMtzz@users.noreply.github.com>
2021-10-23 01:13:22 -0500

    Iniciar sesion y crear cuenta

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import "./App.css";
 //import Welcome from './components/Welcome';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import index from './pages';
-import profile from './pages/profile';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import index from "./pages";
+import profile from "./pages/profile";
+import SignIn from "./pages/sign-in";
+import SignUp from "./pages/sign-up";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={index} />
         <Route exact path="/profile" component={profile} />
+        <Route exact path="/sign-in" component={SignIn} />
+        <Route exact path="/sign-up" component={SignUp} />
       </Switch>
     </Router>
   );
