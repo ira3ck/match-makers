a06c12eb89bd6af00ebb0531564a48328fc5d60f
Bio121 <daniel.alejandro121@gmail.com>
2021-11-19 22:49:42 -0600

    poco avance de configurar usuario

--- src/pages/preferences.js
+++ src/pages/preferences.js
@@ -1,9 +1,22 @@
-import { Box, Button, Container, makeStyles, TextField } from '@material-ui/core';
+import { Box, Button, Container, makeStyles, TextField, Typography } from '@material-ui/core';
 import React from 'react'
 
 const useStyles = makeStyles((theme) => ({
     content: {
         padding: theme.spacing(3),
+    },
+    txtFiel: {
+        display: "block",
+        paddingBottom: theme.spacing(2),
+        paddingTop: theme.spacing(1),
+        width: "15rem",
+        
+      },
+    cuadroTexto:{
+        display: "flex",
+        direction: "column",
+        justifyContent: "flex-start",
+        aligntItems: "stretch",
     },
 }));
 
@@ -12,10 +25,19 @@ const Preferences = () => {
     const classes = useStyles();
     return (
         <Container>
-            <form noValidate autoComplete="off">
-                <Box >
+            <form noValidate autoComplete="off" className={classes.content}>
+                <Box className={classes.cuadroTexto}>
+                    <div><Typography>
+                        Nombre de usuario:
+                        </Typography></div>
                     <TextField id="username" label="Username" variant="outlined" />
+                    <div><Typography>
+                        Nombre(s):
+                    </Typography></div>
                     <TextField id="name" label="Nombre(s)" variant="outlined" />
+                    <div><Typography>
+                        Apellido(s):
+                    </Typography></div>
                     <TextField id="last-name" label="Apellido(s)" variant="outlined" />
                 </Box>
                 <Button
