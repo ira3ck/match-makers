b60cf4dd792cb2e13fc7a66b1ceef6ff7afc045d
JesusMtzz <52387097+JesusMtzz@users.noreply.github.com>
2021-11-20 10:30:36 -0600

    conexion squad y crear squad

--- src/pages/squad.js
+++ src/pages/squad.js
@@ -1,62 +1,110 @@
-import React, { Fragment } from 'react'
-import { Box, Grid, makeStyles } from '@material-ui/core';
-import Headersquad from '../components/headerSquad'
-import IntegrantesSquad from '../components/integrantesSquad'
-import Poste from '../components/posteo'
-import Scrollable from '../components/Scrollable';
+import React, { Fragment } from "react";
+import { Box, Grid, makeStyles } from "@material-ui/core";
+import Headersquad from "../components/headerSquad";
+import IntegrantesSquad from "../components/integrantesSquad";
+import Poste from "../components/posteo";
+import Scrollable from "../components/Scrollable";
+
+import { Link } from "react-router-dom";
+
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
-    espacio: {
-        marginTop: theme.spacing(3),
-    },
-    content: {
-        padding: theme.spacing(3),
-    },
-    BG: {
-        backgroundAttachment: 'fixed',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no - repeat',
-        backgroundSize: 'cover',
-    }
+  espacio: {
+    marginTop: theme.spacing(3),
+  },
+  content: {
+    padding: theme.spacing(3),
+  },
+  BG: {
+    backgroundAttachment: "fixed",
+    backgroundPosition: "center",
+    backgroundRepeat: "no - repeat",
+    backgroundSize: "cover",
+  },
 }));
 
 const Index = () => {
-    const classes = useStyles();
-    const posteo = {
-        categoria: "Tipo de juego",
-        juego: "Nombre del juego",
-        fechaCreacion: "18 noviembre 2021",
-        userIMG: "https://pbs.twimg.com/media/EiNYM5CWAAAh9PV?format=png&name=240x240",
-        sqName: "Supermegasquad",
-        cuerpo: "Hola, Gamer, juguemos League of Legends ¿Qué te parece si lo descargamos en este preciso momento y descubrimos juntos como se juega? Es fácil de aprender, funciona de esta manera: 2 equipos de 5 jugadores deben destruir la base enemiga combatiendo en un mapa lleno de objetivos y monstruos. Supongamos que de entre los mas de 100 campeones Elijes a Garen, te gusta Garen, es fácil matar a tus enemigos con Garen. Al principio no necesitas ser experto por qué el juego te posiciona con otros jugadores de tu mismo nivel, esto significa que podrás mejorar con solo jugar y divertirte. No hay nada como ganar en League of Legends al lado de 4 nuevos aliados... ¿Hermoso, verdad?"
-    }
-    const squad = {
-        squad_name: "Supermegasquad",
-        desc: "Somos los mas sanguinarios del frifayer, nuestro rango? la pregunta ofende nene, puro heroico en el gremio papa Somos los mas sanguinarios del frifayer, nuestro rango? la pregunta ofende nene, puro heroico en el gremio papa Somos los mas sanguinarios del frifayer, nuestro rango? la pregunta ofende nene, puro heroico en el gremio papa Somos los mas sanguinarios del frifayer, nuestro rango? la pregunta ofende nene, puro heroico en el gremio papa Somos los mas sanguinarios del frifayer, nuestro rango? la pregunta ofende nene, puro heroico en el gremio papa",
-        squad_pic: "../img/Spirited-away-ogino-chihiro-studio-ghibli-1920x1080-wallpaper.jpg",
-        background_squad_pic: "../img/654978.jpg",
-    }
-    return (
-        <Fragment >
-            <Box component="div" className={classes.BG} style={{ backgroundImage: `url(${squad.background_squad_pic})` }}>
-                <Box component="div" className={classes.content}>
-                    <Headersquad data={squad} />
-                    <Grid container direction="row" justifyContent="space-evenly" className={classes.spaceTop}>
-                        <Grid item>
-                            <IntegrantesSquad />
-                        </Grid>
-                        <Grid item sm={8}>
-                            <Scrollable>
-                                <Poste posteo={posteo} />
-                                <Poste posteo={posteo} />
-                                <Poste posteo={posteo} />
-                            </Scrollable>
-                        </Grid>
-                    </Grid>
-                </Box>
+  const classes = useStyles();
+  const posteo = {
+    categoria: "Tipo de juego",
+    juego: "Nombre del juego",
+    fechaCreacion: "18 noviembre 2021",
+    userIMG:
+      "https://pbs.twimg.com/media/EiNYM5CWAAAh9PV?format=png&name=240x240",
+    sqName: "Supermegasquad",
+    cuerpo:
+      "Hola, Gamer, juguemos League of Legends ¿Qué te parece si lo descargamos en este preciso momento y descubrimos juntos como se juega? Es fácil de aprender, funciona de esta manera: 2 equipos de 5 jugadores deben destruir la base enemiga combatiendo en un mapa lleno de objetivos y monstruos. Supongamos que de entre los mas de 100 campeones Elijes a Garen, te gusta Garen, es fácil matar a tus enemigos con Garen. Al principio no necesitas ser experto por qué el juego te posiciona con otros jugadores de tu mismo nivel, esto significa que podrás mejorar con solo jugar y divertirte. No hay nada como ganar en League of Legends al lado de 4 nuevos aliados... ¿Hermoso, verdad?",
+  };
+  const squad = {
+    squad_name: "Supermegasquad",
+    desc: "Somos los mas sanguinarios del frifayer, nuestro rango? la pregunta ofende nene, puro heroico en el gremio papa Somos los mas sanguinarios del frifayer, nuestro rango? la pregunta ofende nene, puro heroico en el gremio papa Somos los mas sanguinarios del frifayer, nuestro rango? la pregunta ofende nene, puro heroico en el gremio papa Somos los mas sanguinarios del frifayer, nuestro rango? la pregunta ofende nene, puro heroico en el gremio papa Somos los mas sanguinarios del frifayer, nuestro rango? la pregunta ofende nene, puro heroico en el gremio papa",
+    squad_pic:
+      "../img/Spirited-away-ogino-chihiro-studio-ghibli-1920x1080-wallpaper.jpg",
+    background_squad_pic: "../img/654978.jpg",
+  };
+  return (
+    <Fragment>
+      <Box
+        component="div"
+        className={classes.BG}
+        style={{ backgroundImage: `url(${squad.background_squad_pic})` }}
+      >
+        <Box component="div" className={classes.content}>
+          <Box display="flex" flexDirection="row-reverse" p={1} m={1}>
+            <Box
+              p={1}
+              style={{
+                backgroundColor: "rgba(0, 151, 19, 0.4)",
+                borderRadius: " 10px",
+                alignContent: "center",
+              }}
+            >
+              <Typography
+                style={{
+                  alignContent: "center",
+                }}
+                variant="h6"
+                gutterBottom
+              >
+                No tienes un squad?
+              </Typography>
+              <Button
+                to="/createSquad"
+                component={Link}
+                style={{
+                  width: "100%",
+                  alignContent: "center",
+                }}
+              >
+                Crea uno
+              </Button>
             </Box>
-        </Fragment>
-    )
-}
+          </Box>
+
+          <Headersquad data={squad} />
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-evenly"
+            className={classes.spaceTop}
+          >
+            <Grid item>
+              <IntegrantesSquad />
+            </Grid>
+            <Grid item sm={8}>
+              <Scrollable>
+                <Poste posteo={posteo} />
+                <Poste posteo={posteo} />
+                <Poste posteo={posteo} />
+              </Scrollable>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Fragment>
+  );
+};
 
 export default Index;
