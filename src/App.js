3dd71366911c6c1fa65e0804332152f7e52b232a
JesusMtzz <52387097+JesusMtzz@users.noreply.github.com>
2021-11-20 05:25:42 -0600

    xd

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import SignUp from "./pages/sign-up";
 import squad from "./pages/squad";
 import SideBar from "./components/sideBar";
 import Preferences from "./pages/preferences";
+import createSquad from "./pages/crearSquad";
+
 import { ThemeProvider } from "@material-ui/styles";
 import { createTheme } from "@material-ui/core";
 import PrivateRoute from "./components/session/PrivateRoute";
@@ -17,15 +19,15 @@ import History from "./utils/History";
 
 const theme = createTheme({
   palette: {
-    type: 'dark',
+    type: "dark",
   },
 });
 
 function App() {
   return (
     //<Welcome/>
-    <ThemeProvider theme = {theme}>
-      <Router history= {History}>
+    <ThemeProvider theme={theme}>
+      <Router history={History}>
         <SideBar>
           <Switch>
             <PrivateRoute exact path="/" component={Index} />
@@ -33,8 +35,10 @@ function App() {
             <Route exact path="/sign-in" component={SignIn} />
             <Route exact path="/sign-up" component={SignUp} />
             <Route exact path="/squad" component={squad} />
-            <Route path="/login" component={Login} exact/>
-            <Route path="/logout" component={Logout} exact/>
+            <Route path="/login" component={Login} exact />
+            <Route path="/logout" component={Logout} exact />
+            <Route exact path="/preferences" component={Preferences} />
+            <Route exact path="/createSquad" component={createSquad} />
           </Switch>
         </SideBar>
       </Router>
--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,186 +1,244 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import { Link } from 'react-router-dom';
-import { Avatar, Box, Divider, Grid, Paper, Popover, Typography } from '@material-ui/core';
-import LogoIcon from './LogoIcon';
-import { GroupOutlined, HomeOutlined, MoreHoriz, PersonOutlined, PowerSettingsNew, Settings } from '@material-ui/icons';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import { Link } from "react-router-dom";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Grid,
+  Paper,
+  Popover,
+  Typography,
+} from "@material-ui/core";
+import LogoIcon from "./LogoIcon";
+import {
+  GroupOutlined,
+  HomeOutlined,
+  PersonOutlined,
+  PowerSettingsNew,
+  Settings,
+} from "@material-ui/icons";
 
+const drawerWidth = 260;
 
-import { create } from '../api/MMapi';
+const RegularFunction = async () => {
+  const TestObj = {
+    name: "KEK",
+    last_name: "ZV",
+    username: "MomoZV",
+    email: "momo@gmail.com",
+  };
 
-const drawerWidth = 360;
-
-const  RegularFunction =  async ()=>{
-    const TestObj = { name:"KEK", last_name: "ZV", username:"MomoZV", email:"momo@gmail.com"};
-
-    await create(TestObj);
+  await create(TestObj);
 };
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    alineadoDerecha: {
-        display: 'flex',
-        justifyContent: 'flex-end',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    coloredText: {
-        color: 'aliceblue',
-    },
-    drawerPaper: {
-        width: drawerWidth,
-        backgroundColor: 'rgb(0,0,0)',
-        paddingRight: theme.spacing(3),
-    },
-    itemList: {
-        marginBottom: theme.spacing(3),
-        borderRadius: "50px",
-    },
-    toolbar: theme.mixins.toolbar,
-    content: {
-        backgroundColor: theme.palette.background.default,
-    },
-    paperOptions: {
-        backgroundColor: '#000',
-        boxShadow: '0 10px 20px aliceblue',
-        WebkitBoxShadow: '0 10px 20px aliceblue',
-        MozBoxShadow: '0 10px 20px aliceblue',
-    },
+  root: {
+    display: "flex",
+  },
+  alineadoDerecha: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  coloredText: {
+    color: "aliceblue",
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    backgroundColor: "rgb(0,0,0)",
+    paddingRight: theme.spacing(3),
+  },
+  itemList: {
+    marginBottom: theme.spacing(3),
+    borderRadius: "50px",
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    backgroundColor: theme.palette.background.default,
+  },
+  paperOptions: {
+    backgroundColor: "#000",
+    boxShadow: "0 10px 20px aliceblue",
+    WebkitBoxShadow: "0 10px 20px aliceblue",
+    MozBoxShadow: "0 10px 20px aliceblue",
+  },
 }));
 
 export default function SideBar(props) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const profile = {
-        profile_pic: '../img/654978.jpg',
-        name: 'ira3ck',
-        squad: 'Supermegasquad'
-    }
+  const profile = {
+    profile_pic: "../img/654978.jpg",
+    name: "ira3ck",
+    squad: "Supermegasquad",
+  };
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const open = Boolean(anchorEl);
-    const id = open ? 'simple-popover' : undefined;
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
 
-    return (
-        <Box sx={{ display: "flex" }}>
-            <CssBaseline />
-            <Drawer
-                className={classes.drawer}
-                variant="permanent"
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-                anchor="left"
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+
+        <Grid
+          container
+          direction="column"
+          justifyContent="space-between"
+          alignItems="flex-end"
+          style={{ height: "100%" }}
+        >
+          <Grid item>
+            <List>
+              <ListItem className={classes.itemList}>
+                <LogoIcon className={classes.LogoIcon}></LogoIcon>
+              </ListItem>
+
+              <ListItem
+                button
+                key="Home"
+                component={Link}
+                to="/"
+                className={classes.itemList}
+              >
+                <ListItemIcon className={classes.coloredText}>
+                  <HomeOutlined />
+                </ListItemIcon>
+                <ListItemText className={classes.coloredText} primary="Home" />
+              </ListItem>
+
+              <ListItem
+                button
+                key="Squad"
+                component={Link}
+                to="/squad"
+                className={classes.itemList}
+              >
+                <ListItemIcon className={classes.coloredText}>
+                  <GroupOutlined />
+                </ListItemIcon>
+                <ListItemText className={classes.coloredText} primary="Squad" />
+              </ListItem>
+
+              <ListItem
+                button
+                key="Profile"
+                component={Link}
+                to="/profile"
+                className={classes.itemList}
+              >
+                <ListItemIcon className={classes.coloredText}>
+                  <PersonOutlined />
+                </ListItemIcon>
+                <ListItemText
+                  className={classes.coloredText}
+                  primary="Profile"
+                />
+              </ListItem>
+            </List>
+          </Grid>
+
+          <Grid item>
+            <ListItem
+              button
+              className={classes.itemList}
+              aria-describedby={id}
+              onClick={handleClick}
             >
-                <div className={classes.toolbar} />
-
-                <Grid
-                    container
-                    direction="column"
-                    justifyContent="space-between"
-                    alignItems="flex-end"
-                    style={{ height: "100%" }}
-                >
-                    <Grid item>
-                        <List>
-                            <ListItem className={classes.itemList}>
-                                <LogoIcon className={classes.LogoIcon}></LogoIcon>
-                            </ListItem>
-
-                            <ListItem button key='Home' component={Link} to='/' className={classes.itemList}>
-                                <ListItemIcon className={classes.coloredText}><HomeOutlined /></ListItemIcon>
-                                <ListItemText className={classes.coloredText} primary='Home' />
-                            </ListItem>
-
-                            <ListItem button key='Squad' component={Link} to='/squad' className={classes.itemList}>
-                                <ListItemIcon className={classes.coloredText}><GroupOutlined /></ListItemIcon>
-                                <ListItemText className={classes.coloredText} primary='Squad' />
-                            </ListItem>
-
-                            <ListItem button key='Profile' component={Link} to='/profile' className={classes.itemList}>
-                                <ListItemIcon className={classes.coloredText}><PersonOutlined /></ListItemIcon>
-                                <ListItemText className={classes.coloredText} primary='Profile' />
-                            </ListItem>
-                        </List>
-                    </Grid>
-
-                    <Grid item>
-                        <ListItem button className={classes.itemList} aria-describedby={id} onClick={handleClick}>
-                            <Grid
-                                container
-                                direction="row"
-                                alignItems="center"
-                            >
-                                <Grid item>
-                                    <Avatar alt='Profile Pic' src={profile.profile_pic}></Avatar>
-                                </Grid>
-                                <Grid item>
-                                    <Typography style={{ marginLeft: "15px" }}>{profile.name}</Typography>
-                                    <Typography style={{ marginLeft: "15px", fontSize: 'small' }}>{profile.squad}</Typography>
-                                </Grid>
-                                <Grid>
-                                    <MoreHoriz style={{marginLeft: '25px'}}/>
-                                </Grid>
-                            </Grid>
-                        </ListItem>
-                    </Grid>
+              <Grid container direction="row" alignItems="center">
+                <Grid item>
+                  <Avatar alt="Profile Pic" src={profile.profile_pic}></Avatar>
                 </Grid>
+                <Grid item>
+                  <Typography style={{ marginLeft: "15px" }}>
+                    {profile.name}
+                  </Typography>
+                  <Typography style={{ marginLeft: "15px", fontSize: "small" }}>
+                    {profile.squad}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </ListItem>
+          </Grid>
+        </Grid>
 
-                <div className={classes.toolbar} />
+        <div className={classes.toolbar} />
+      </Drawer>
+      <Box component="main" className={classes.content}>
+        {props.children}
+      </Box>
 
-            </Drawer>
-            <Box component="main" className={classes.content}>
-                {props.children}
-            </Box>
-
-            <Popover
-                id={id}
-                open={open}
-                anchorEl={anchorEl}
-                onClose={handleClose}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                transformOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right',
-                }}
-
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+      >
+        <Paper className={classes.paperOptions}>
+          <List>
+            <ListItem
+              button
+              key="Preferences"
+              component={Link}
+              to="/preferences"
             >
-                <Paper className={classes.paperOptions}>
-                    <List>
-                        <ListItem button key='Preferences' component={Link} to='/preferences' onClick={handleClose}>
-                            <ListItemIcon className={classes.coloredText}><Settings /></ListItemIcon>
-                            <ListItemText className={classes.coloredText} primary='Preferencias' />
-                        </ListItem>
-                        <Divider />
-                        <ListItem button key='Logout' component={Link} to='/' onClick={handleClose}>
-                            <ListItemIcon className={classes.coloredText}><PowerSettingsNew /></ListItemIcon>
-                            <ListItemText className={classes.coloredText} primary='Cerrar sesión' />
-                        </ListItem>
-                    </List>
-                </Paper>
-            </Popover>
-        </Box>
-    );
+              <ListItemIcon className={classes.coloredText}>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText
+                className={classes.coloredText}
+                primary="Preferencias"
+              />
+            </ListItem>
+            <Divider />
+            <ListItem button key="Logout" component={Link} to="/">
+              <ListItemIcon className={classes.coloredText}>
+                <PowerSettingsNew />
+              </ListItemIcon>
+              <ListItemText
+                className={classes.coloredText}
+                primary="Cerrar sesión"
+              />
+            </ListItem>
+          </List>
+        </Paper>
+      </Popover>
+    </Box>
+  );
 }
