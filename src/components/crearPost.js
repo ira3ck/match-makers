afbc0522a276518a24a48d478c9cc3c2f6d876b1
JesusMtzz <52387097+JesusMtzz@users.noreply.github.com>
2021-11-20 05:08:08 -0600

    crearpost

--- src/components/crearPost.js
+++ src/components/crearPost.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
@@ -9,6 +9,10 @@ import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 
 import Typography from "@material-ui/core/Typography";
+import { PostAdd } from "@material-ui/icons";
+
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -18,71 +22,114 @@ const useStyles = makeStyles((theme) => ({
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
+  button: {
+    margin: theme.spacing(1),
+  },
 }));
 
 const CrearPost = () => {
   const classes = useStyles();
-  const [juego, setJuego] = React.useState("");
 
-  const handleChange = (event) => {
-    setJuego(event.target.value);
+  const [post, setPost] = useState({
+    creador: "",
+    permanencia: "",
+    juego: "",
+    cuerpo: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPost({
+      ...post,
+      [name]: value,
+    });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div
       style={{
         width: "100%",
-        margin: "10px",
+        margin: "1.2%",
         borderRadius: "10px",
         padding: "15px",
         alignContent: "center",
       }}
     >
-      <Box display="flex" p={1}>
-        <Box p={1} flexGrow={1}>
-          <Box>
-            <Typography variant="h3" gutterBottom>
-              Crear Post
-            </Typography>
+      <form onSubmit={handleSubmit}>
+        <Box display="flex" p={1}>
+          <Box p={1} flexGrow={1}>
+            <Box>
+              <Typography variant="h3" gutterBottom>
+                Crear Post
+              </Typography>
+            </Box>
+
+            <Box>
+              <FormControl className={classes.formControl}>
+                <InputLabel id="selectJuego-label">Juego</InputLabel>
+                <Select
+                  name="juego"
+                  labelId="selectJuego-label"
+                  id="selectJuego"
+                  required
+                  value={post.juego}
+                  onChange={handleChange}
+                >
+                  <MenuItem value={"Apex Legends"}>Apex Legends</MenuItem>
+                  <MenuItem value={"Rocket league"}>Rocket League</MenuItem>
+                  <MenuItem value={"Warzone"}>Warzone</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
           </Box>
-          <Box>
-            <FormControl className={classes.formControl}>
-              <InputLabel id="selectJuego-label">Juego</InputLabel>
-              <Select
-                labelId="selectJuego-label"
-                id="selectJuego"
-                value={juego}
-                onChange={handleChange}
-              >
-                <MenuItem value={"Apex Legends"}>Apex Legends</MenuItem>
-                <MenuItem value={"Rocket league"}>Rocket League</MenuItem>
-                <MenuItem value={"Warzone"}>Warzone</MenuItem>
-              </Select>
-            </FormControl>
+
+          <Box p={1}>
+            <TextField
+              style={{ float: "right" }}
+              id="filled-number"
+              name="permanencia"
+              label="Horas de duracion del post"
+              type="number"
+              value={post.permanencia}
+              onChange={handleChange}
+              required
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
           </Box>
         </Box>
-        <Box p={1}>
+        <Box display="flex" p={1}>
           <TextField
-            style={{ float: "right" }}
-            id="filled-number"
-            label="Horas de duracion del post"
-            type="number"
-            InputLabelProps={{
-              shrink: true,
-            }}
+            style={{ width: "100%" }}
+            id="filled-textarea"
+            name="cuerpo"
+            label="Mensaje"
+            required
+            placeholder="ej. Quien para un lolsito"
+            multiline
+            value={post.cuerpo}
+            onChange={handleChange}
+            variant="filled"
           />
         </Box>
-      </Box>
 
-      <Box display="flex" p={1}>
-        <TextField
-          style={{ width: "100%" }}
-          id="filled-textarea"
-          label="Mensaje"
-          placeholder="ej. Quien para un lolsito"
-          multiline
-          variant="filled"
-        />
-      </Box>
+        <Box display="flex" flexDirection="row-reverse" m={1}>
+          <Box>
+            <Button
+              variant="contained"
+              className={classes.button}
+              endIcon={<Icon>send</Icon>}
+            >
+              Publicar
+            </Button>
+          </Box>
+        </Box>
+      </form>
     </div>
   );
 };
